c98e08e2468d11bbd050042fe399fb746036192d
Kirk Webb <kwebb@cs.utah.edu>
2019-11-22 18:28:27 -0700

    Updates to gnr environment.

--- startup.sh
+++ startup.sh
@@ -15,4 +15,15 @@ do
     esac
 done
 
-sudo "/usr/lib/uhd/utils/uhd_images_downloader.py"
+sudo sysctl -w net.core.rmem_max=24862979
+sudo sysctl -w net.core.wmem_max=24862979
+
+sudo ed /etc/sysctl.conf << "EDEND"
+a
+net.core.rmem_max=24862979
+net.core.wmem_max=24862979
+.
+w
+EDEND
+
+#sudo "/usr/lib/uhd/utils/uhd_images_downloader.py -t x310"
