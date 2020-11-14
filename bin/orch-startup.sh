e2674651918caa75e5f7f735f9dc7417a2e5b077
Kirk Webb <kwebb@cs.utah.edu>
2020-11-14 09:23:54 -0700

    Add manual route to FE VPN.

--- bin/orch-startup.sh
+++ bin/orch-startup.sh
@@ -8,6 +8,8 @@ cd $REPODIR
 git submodule update --init --remote || \
     { echo "Failed to update git submodules!" && exit 1; }
 
+sudo ip route add 155.98.47.0/24 via 155.98.36.204
+
 $SHOUTSRC/orchestrator.py -d
 
 exit 0
