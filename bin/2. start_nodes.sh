8e908cde93d04bb93853db0ff88bf4fce706e0ef
Aniqua Baset <aniqua@Aniquas-MacBook-Pro.local>
2021-02-17 23:59:06 -0700

    Updated bin and cmd files

--- bin/2. start_nodes.sh
+++ bin/2. start_nodes.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 ip="155.98.37.214"
-txrx=false
 
-now=$(date +"%m_%d_%Y_%I_%M_%S")
-out="/var/emulab/save/run_$now"
-#out="/var/emulab/save/band7_dl_nuc1_test"
+today=$(date +"%m_%d_%Y)
+now=$(date +"%T")
+
+out="/var/emulab/save/$today"
+out="/var/emulab/save/$today/$now"
 mkdir "$out"
-cd /local/repository/shout
 
-echo $ip
-if [ "$txrx" != true ] ; then 
-	echo "Not tx/rx"
-	python3 meascli.py -p 2000 -l "$out/log" -s $ip
-else
-	python3 meascli.py -t -p 2000 -l "$out/log" -s $ip
-fi
+cd /local/repository/shout
+python3 meascli.py -p 2000 -l "$out/log" -s $ip
--- bin/3. run_orch.sh
+++ bin/3. run_orch.sh
@@ -1,30 +1,17 @@
 #!/bin/bash
 
 ip="155.98.37.214"
-#cmd="cbrs_tx_sine"
-#cmd="cbrs_tx_sine_rx"
-#cmd="cbrs_tx_sine_rx_rev"
-#cmd="cbrs_meas"
-#cmd="cell_rx_recv"
-#cmd="cell_power_var_meas"
-#cmd="cell_power_var_meas_test"
-#cmd="cell_power_var_meas_sine"
-cmd="find_lte"
-#cmd="band7_dl_nuc1_nuc2_test"
-#cmd="cbrs_tx_rx"
-#cmd="bus_rx"
 
+cmd="bus_tx_rx"
 
-
-now=$(date +"%m_%d_%Y_%I_%M_%S")
-out="/var/emulab/save/run_$now"
-#out="/var/emulab/save/band7_dl_nuc1_test"
-
+today=$(date +"%m_%d_%Y)
+now=$(date +"%T")
+out="/var/emulab/save/$today"
+out="/var/emulab/save/$today/$now"
 mkdir "$out"
-mkdir "$out/data"
 
 cmd_file="/local/repository/etc/cmd_files/$cmd.json"
-cp  $cmd_file "$out/data/$cmd.json"
+cp  $cmd_file "$out/$cmd.json"
 
 cd /local/repository/shout
-python3 measiface.py -p 2000 -l "$out/log" -s $ip -o "$out/data" -c $cmd_file
+python3 measiface.py -p 2000 -l "$out/" -s $ip -o "$out/" -c $cmd_file
