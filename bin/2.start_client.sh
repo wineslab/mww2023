f959ffc83451848477780e78ea35c302d83822ca
Frost Mitchell <fmitch@cs.utah.edu>
2022-12-30 09:05:10 -0700

    Update shout

--- bin/2.start_client.sh
+++ bin/2.start_client.sh
@@ -33,6 +33,6 @@ sudo rm $CLILOG
 ORCHDOM=`$REPODIR/bin/getexpdom.py`
 ORCHHOST="$ORCHNAME.$ORCHDOM"
 echo "$ORCHHOST"
-$SHOUTSRC/meascli.py -s $ORCHHOST #--useoffset
+$SHOUTSRC/meascli.py -xc -s $ORCHHOST #--useoffset
 
 exit 0
