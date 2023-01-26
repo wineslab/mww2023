ff9ab9586ec65e540aa2d2ec54870ea281c597d3
Davide Villa <villus@hotmail.it>
2023-01-26 14:59:07 -0700

    Updating run_cmd to gold

--- bin/3.run_cmd.sh
+++ bin/3.run_cmd.sh
@@ -13,7 +13,8 @@ for i in $(eval echo "{$START..$REPEAT}")
 do
 	#for CMD in "save_iq_w_simult_tx_1" "save_iq_w_simult_tx_2" "save_iq_w_simult_tx_3"
 	#for CMD in "save_iq_w_simult_tx_2" "save_iq_w_simult_tx_3"
-	for CMD in "save_iq_w_tx_cw"
+	#for CMD in "save_iq_w_tx_cw"
+	for CMD in "save_iq_w_tx_gold"
 	do
 	
 		today=$(date +"%m-%d-%Y")
